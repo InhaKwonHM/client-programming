6a945d9c4aa94c099f16a02ef71e6cc32e2c1a82
InhaKwonHM <gudalsdl0421@inha.edu>
2025-05-24 11:14:37 +0900

    2025-05-24-02

---
+++ src/components/user/CartPage.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react'
+import {app} from '../../firebase'
+import {getDatabase, ref, onValue, remove} from 'firebase/database'
+import { Button, Table } from 'react-bootstrap';
+import BookPage from '../BookPage';
+import { RiDeleteBin2Line } from "react-icons/ri";
+
+const CartPage = () => {
+    const uid = sessionStorage.getItem('uid');
+    const [loading, setLoading] = useState(false);
+    const [books,setBooks] = useState([]);
+    const db = getDatabase(app);
+    const getCart = () => {
+        setLoading(true)
+        onValue(ref(db,`cart/${uid}`), snapshot=>{
+            const rows=[]
+            snapshot.forEach(row=>{
+                rows.push({key:row.key, ...row.val()})
+            })
+            //console.log(rows);
+            setBooks(rows);
+            setLoading(false);
+        });
+    }
+
+    useEffect(() => {
+        getCart();
+    },[]);
+    const onClickRemove = (book) => {
+        if(window.confirm(`'${book.title}' 을(를) 삭제하시겠습니까?`)) {
+            remove(ref(db, `cart/${uid}/${book.isbn}`));
+        }
+    }
+
+    if(loading) return <h1 className='my-5 text-center'>로딩중......</h1>
+    return (
+        <div>
+            <h1 className='my-4 text-center'>장바구니</h1>
+            <Table>
+                <thead>
+                    <tr>
+                        <td></td>
+                        <td>제목</td>
+                        <td>등록일</td>
+                        <td>삭제</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {books.map(book=>
+                        <tr key={book.isbn}>
+                            <td width={50}><BookPage book={book}/></td>
+                            <td>{book.title}</td>
+                            <td>{book.date}</td>
+                            <td><Button variant='outline-danger' size="sm" onClick={()=>onClickRemove(book)}><RiDeleteBin2Line   size="20"/></Button></td>
+                        </tr>
+                    )}
+                </tbody>
+            </Table>
+        </div>
+    )
+}
+
+export default CartPage
--- src/components/user/LoginPage.jsx
+++ src/components/user/LoginPage.jsx
@@ -1,24 +1,28 @@
 import React, { useState } from 'react'
 import { Button, Card, Col, Form, Row } from 'react-bootstrap'
 import { app } from '../../firebase'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 
 const LoginPage = () => {
     const auth = getAuth(app); // Firebase 인증
+    const [loading, setLoading] = useState(false);
+    const navi = useNavigate();
     const basename = process.env.PUBLIC_URL;
 
     const [form, setForm] = useState({
         email: 'dark@inha.com',
-        pass: 'password'
+        pass: '12341234'
     });
 
-    const { email, pass } = form;
 
     const onChange = (e) => {
         setForm({
-            ...Form,
+            ...form,
             [e.target.name]: e.target.value
         });
     };
+    const { email, pass } = form;
 
     const onSubmit = (e) => {
         e.preventDefault();
@@ -29,16 +33,22 @@ const LoginPage = () => {
             alert('이메일 or 패스워드를 입력하세요!')
         } else {
             // 로그인 체크
+            setLoading(true);
             signInWithEmailAndPassword(auth, email, pass)
             .then(success => {
                 alert('로그인 성공')
+                sessionStorage.setItem('email',email);
+                sessionStorage.setItem('uid',success.user.uid);
+                setLoading(false);
+                navi('/');
             })
             .catch(error=>{
-                alert('로그인 에러')
+                alert('로그인 에러' + error.message)
+                setLoading(false);
             })
         }
     }
-
+    if(loading) return <h1 className='my-5 text-center'>로딩중......</h1>
 
     return (
         <div>
